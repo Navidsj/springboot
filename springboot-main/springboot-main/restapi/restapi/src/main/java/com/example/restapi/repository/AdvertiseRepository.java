1e10df64e057ac33f3a57cd1f63485595a67b63b
navidsj <navidsj36@gmail.com>
2024-08-18 12:41:49 -0700

    file ezafe hazf shod

--- springboot-main/springboot-main/restapi/restapi/src/main/java/com/example/restapi/repository/AdvertiseRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.restapi.repository;
-
-import com.example.restapi.model.Advertise;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AdvertiseRepository extends JpaRepository<Advertise, Long> {
-}
